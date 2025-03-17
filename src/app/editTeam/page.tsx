8e1b40e30249edf285a57ba8178588526fbcafa6
Mariana S Oliveira <marianasoli621@gmail.com>
2025-03-17 20:47:57 -0300

    correções nas fontes

--- src/app/editTeam/page.tsx
+++ src/app/editTeam/page.tsx
@@ -37,7 +37,7 @@ export default function Team() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-6">Nossa Equipe</h1>
+      <h1 className="text-3xl font-dm-serif font-bold text-center  mb-6">Nossa Equipe</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {employees.map((employee) => (
           <div key={employee.id} className="bg-white shadow-lg rounded-lg p-4">
