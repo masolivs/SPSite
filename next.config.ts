0f3ccd01f6ce6a85e120fa86d87352c17f10ff09
Mariana S Oliveira <marianasoli621@gmail.com>
2025-03-16 21:21:14 -0300

    correção na configuração de imagens no next

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['isyftrxpmaejonlpuiwd.supabase.co'], 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'isyftrxpmaejonlpuiwd.supabase.co', 
+        port: '', 
+        pathname: '/storage/v1/object/public/**', 
+      },
+    ],
   },
 };
 
